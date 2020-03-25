bdb740aa2afb22093eeba912d29ba8f8afdc14eb
dumota <edumota02@gmail.com>
2020-03-25 02:07:17 -0300

    upando o jogo compactado

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1585111685,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/chao-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/ceu.png",
+		"images/sprite2-sheet0.png",
+		"images/basecanhao-sheet0.png",
+		"images/potacanhao-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/box-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
